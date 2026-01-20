f672442b7251a9bbbf0b9727a2df2e954745dba8
Sukasa <sukasa@greenfirework.com>
2026-01-20 11:12:19 -0800

    Expand symbol manager to handle creation of scopes and better symbol resolution / addition

--- SymbolManager.cs
+++ SymbolManager.cs
@@ -1,17 +1,23 @@
 ﻿using static IC10_Inliner.IC10Program;
+using static IC10_Inliner.Symbol;
 
 namespace IC10_Inliner;
 
 internal class SymbolManager
 {
-    private List<IList<Symbol>> Scopes = new();
-    private List<int> Offsets = new();
+    private readonly List<IList<Symbol>> Scopes = [];
+    private readonly List<int> Offsets = [];
 
     public void AddSymbol(Symbol NewSymbol)
     {
-        // TODO Check for collisions
+        if (Scopes[^1].Any(x => x.SymbolName == NewSymbol.SymbolName))
+            throw new Exception($"Duplicate symbol definition {NewSymbol.SymbolName}");
 
-        Scopes[Scopes.Count - 1].Add(NewSymbol);
+        // TODO this needs to be a warning, not an error.  That will have to come a bit later.
+        if (Scopes[0..^1].Any(x => x.Any(y => y.SymbolName == NewSymbol.SymbolName)))
+            throw new Exception($"Symbol definition {NewSymbol.SymbolName} shadows existing symbol");
+
+        Scopes[^1].Add(NewSymbol);
     }
 
     /// <summary>
@@ -24,10 +30,28 @@ internal class SymbolManager
     {
         for (int i = Scopes.Count - 1; i >= 0; i--)
             for (int j = 0; j < Scopes[i].Count; j++)
-                if (Scopes[i][j].IsLabelInScope(Line.OriginalCodeLine))
+                if (Scopes[i][j].SymbolName == SymbolName && (Scopes[i][j].SymbolType != SymbolKind.Label || Scopes[i][j].IsLabelInScope(Line.OriginalCodeLine)))
                     return (Scopes[i][j], Offsets[i]);
+            
 
         throw new Exception($"Unable to resolve symbol {SymbolName}");
+    }
+
+    public void InstantiateScope(IList<Symbol> OriginalScope, int ScopeOffset = 0)
+    {
+        PushScope(OriginalScope, ScopeOffset);
+    }
+
+    /// <summary>
+    ///     Create a new symbol scope, providing an offset to apply to labels within the scope
+    /// </summary>
+    /// <param name="ScopeOffset"></param>
+    /// <returns></returns>
+    public IList<Symbol> CreateScope(int ScopeOffset = 0)
+    {
+        IList<Symbol> NewScope = [];
+        PushScope(NewScope, ScopeOffset);
+        return NewScope;
     }
 
     /// <summary>
