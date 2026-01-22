ed63e646f06200768e299041f237d14ab4c8a066
sukasa <nick.m@shaw.ca>
2026-01-21 19:30:02 -0800

    Plumb in basis of Calculation macro

---
+++ Calculation.cs
@@ -0,0 +1,7 @@
+namespace IC10_Inliner;
+
+public class Calculation
+{
+    // Calculation class.  Given a string, this class tokenizes it, performs symbol substitutions, runs math, and determines a constant result value
+    //
+}
