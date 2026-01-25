b78a62d5d07550f49e0d934f8243d7f865954401
sukasa <nick.m@shaw.ca>
2026-01-24 20:39:42 -0800

    Update example

--- Examples/calculation.asm
+++ Examples/calculation.asm
@@ -1,4 +1,10 @@
-  define someConstant        5
-  define STRIDE              4
+  define  SP_TABLE_START  20
+  define  SP_TABLE_ENTRY_STRIDE 10
 
-  move r0 Calc(someConstant*STRIDE+1)
+macro TableEntry Num Value1 Value2
+  poke Calc(SP_TABLE_ENTRY_STRIDE*Num+SP_TABLE_START) Value1
+  poke Calc(SP_TABLE_ENTRY_STRIDE*Num+SP_TABLE_START+1) Value1
+endmacro
+
+  TableEntry 0 11 22
+  TableEntry 2 33 44
