75fc82cc59986f39a0e2312c65cbcd4151d3e3ab
sukasa <nick.m@shaw.ca>
2026-03-15 11:54:28 -0700

    Add symbol resolve to symbolManager

--- SymbolManager.cs
+++ SymbolManager.cs
@@ -1,33 +1,85 @@
-﻿using static IC10_Inliner.IC10Program;
+﻿using System.Diagnostics.CodeAnalysis;
+using static IC10_Inliner.IC10Program;
 
 namespace IC10_Inliner;
 
 internal class SymbolManager
 {
-    private List<IList<Symbol>> Scopes = new();
-    private List<int> Offsets = new();
+    private readonly List<IList<Symbol>> Scopes = [];
+    private readonly List<int> Offsets = [];
 
     public void AddSymbol(Symbol NewSymbol)
     {
         // TODO Check for collisions
 
-        Scopes[Scopes.Count - 1].Add(NewSymbol);
+        Scopes[^1].Add(NewSymbol);
     }
 
+    public double Resolve(string Token, ProgramLine Line, out Symbol.SymbolKind SymbolKind)
+    {
+        SymbolKind = Symbol.SymbolKind.Constant;
+        
+        if (double.TryParse(Token, out var Result))
+            return Result;
+        
+        var ResultValueString = Token;
+        
+        if (TryGetSymbol(Token, Line, out var FoundSymbol))
+        {
+            ResultValueString = FoundSymbol.Value.Symbol.Resolve();
+            SymbolKind = FoundSymbol.Value.Symbol.SymbolType;
+        }
+        
+        if (Symbol.TryParseBinary(ResultValueString, out var Value) || Symbol.TryParseHex(ResultValueString, out Value))
+            return Value;
+        
+        if (ResultValueString.StartsWith("hash", StringComparison.OrdinalIgnoreCase))
+            ResultValueString = IC10Assembler.ComputeHash(ResultValueString[6..^2]).ToString();
+        
+        else if (ResultValueString.StartsWith("str", StringComparison.OrdinalIgnoreCase))
+            return IC10Assembler.ComputeString(ResultValueString[5..^2]);
+        
+        else if (ResultValueString.StartsWith("calc", StringComparison.OrdinalIgnoreCase))
+                return Calculation.Calculate(ResultValueString[5..^1], Line.Scope);
+
+        return double.Parse(ResultValueString);
+    }
+    
     /// <summary>
     ///     Get a symbol from the store, taking into account current scope and program location
     /// </summary>
     /// <param name="SymbolName"></param>
-    /// <param name="ProgramLine"></param>
+    /// <param name="Line"></param>
     /// <returns></returns>
     public (Symbol Symbol, int Offset) GetSymbol(string SymbolName, ProgramLine Line)
     {
         for (int i = Scopes.Count - 1; i >= 0; i--)
-            for (int j = 0; j < Scopes[i].Count; j++)
-                if (Scopes[i][j].IsLabelInScope(Line.OriginalCodeLine))
-                    return (Scopes[i][j], Offsets[i]);
+        for (int j = 0; j < Scopes[i].Count; j++)
+            if (Scopes[i][j].IsLabelInScope(Line.OriginalCodeLine))
+                return (Scopes[i][j], Offsets[i]);
 
         throw new Exception($"Unable to resolve symbol {SymbolName}");
+    }
+    
+    /// <summary>
+    ///     try to get a symbol from the store, taking into account current scope and program location
+    /// </summary>
+    /// <param name="SymbolName"></param>
+    /// <param name="Line"></param>
+    /// <param name="Result"></param>
+    /// <returns></returns>
+    public bool TryGetSymbol(string SymbolName, ProgramLine Line, [NotNullWhen(true)] out (Symbol Symbol, int Offset)? Result)
+    {
+        for (int i = Scopes.Count - 1; i >= 0; i--)
+        for (int j = 0; j < Scopes[i].Count; j++)
+            if (Scopes[i][j].IsLabelInScope(Line.OriginalCodeLine))
+            {
+                Result = (Scopes[i][j], Offsets[i]);
+                return true;
+            }
+        
+        Result = null;
+        return false;
     }
 
     /// <summary>
